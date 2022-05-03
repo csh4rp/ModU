d1e17c3c8988281b0c70e05a5397577f5455eda1
csh4rp <bieszczyk2@gmail.com>
2022-05-03 17:20:34 +0200

    Added Decorator extensions

---
+++ src/ModU.Infrastructure/Commands/Decorators/TransactionalDecorator.cs
@@ -0,0 +1,15 @@
+﻿using ModU.Abstract.Commands;
+
+namespace ModU.Infrastructure.Commands.Decorators;
+
+internal sealed class TransactionalDecorator<TCommand> : ICommandHandler<TCommand> where TCommand : ICommand
+{
+    private readonly ICommandHandler<TCommand> _handler;
+
+    public TransactionalDecorator(ICommandHandler<TCommand> handler) => _handler = handler;
+
+    public async Task HandleAsync(TCommand command, CancellationToken cancellationToken = new())
+    {
+        await _handler.HandleAsync(command, cancellationToken);
+    }
+}
---
+++ src/ModU.Infrastructure/DependencyInjection/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace ModU.Infrastructure.DependencyInjection;
+
+public static class Extensions
+{
+    public static IServiceCollection Decorate<TService, TDecorator>(this IServiceCollection serviceCollection)
+    {
+        var serviceToWrap = serviceCollection.FirstOrDefault(s => s.ServiceType == typeof(TService));
+        if (serviceToWrap is null)
+        {
+            throw new InvalidOperationException($"Service of type: '{typeof(TService)}' is not registered.");
+        }
+
+        var factory = ActivatorUtilities.CreateFactory(typeof(TDecorator), new[] {typeof(TService)});
+
+        serviceCollection.Replace(ServiceDescriptor.Describe(typeof(TService),
+            s => (TService) factory(s, new[] {s.CreateInstance(serviceToWrap)}), serviceToWrap.Lifetime));
+        
+        return serviceCollection;
+    }
+
+    private static object CreateInstance(this IServiceProvider serviceProvider, ServiceDescriptor serviceDescriptor)
+    {
+        if (serviceDescriptor.ImplementationInstance is not null)
+        {
+            return serviceDescriptor.ImplementationInstance;
+        }
+
+        return serviceDescriptor.ImplementationFactory is not null 
+            ? serviceDescriptor.ImplementationFactory(serviceProvider)
+            : ActivatorUtilities.GetServiceOrCreateInstance(serviceProvider, serviceDescriptor.ImplementationType);
+    }
+}
