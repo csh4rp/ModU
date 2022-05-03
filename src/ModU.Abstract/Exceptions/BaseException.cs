1290d2831502d9d0a3cf9ad453a9783d1c12c704
csh4rp <bieszczyk2@gmail.com>
2022-05-03 16:55:24 +0200

    Added Exception abstractions

---
+++ src/ModU.Abstract/Exceptions/BaseException.cs
@@ -0,0 +1,10 @@
+﻿namespace ModU.Abstract.Exceptions;
+
+public abstract class BaseException : Exception
+{
+    protected BaseException(string message) : base(message)
+    {
+    }
+    
+    public abstract string Code { get; }
+}
