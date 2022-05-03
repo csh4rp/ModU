eb8e1f5c1fade38e674bc0a93f73cc1871e15ea4
csh4rp <bieszczyk2@gmail.com>
2022-05-03 13:53:06 +0200

    Added module abstraction

---
+++ src/ModU.Abstract/Modules/IModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ModU.Abstract.Modules;
+
+public interface IModule
+{
+    string Name { get; }
+
+    void Use(IApplicationBuilder app);
+
+    void Register(IServiceCollection serviceCollection, IConfiguration configuration);
+}
