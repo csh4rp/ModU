77f056aad820c017103f3efc33362d6c2e430196
csh4rp <bieszczyk2@gmail.com>
2022-05-23 21:26:44 +0200

    Added DomainEventStore

---
+++ src/ModU.Infrastructure/Events/Stores/IDomainEventStore.cs
@@ -0,0 +1,8 @@
+﻿using ModU.Abstract.Domain;
+
+namespace ModU.Infrastructure.Events.Stores;
+
+public interface IDomainEventStore
+{
+    Task SaveAsync(Guid aggregateId, Type aggregateType, IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = new());
+}
